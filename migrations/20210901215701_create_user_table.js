455c947de94418cdedf599d3034cde60b5d9c309
satir1313 <shahabj13@gmail.com>
2021-09-01 22:24:15 +0800

    knex migrates to heroku V2

--- migrations/20210901215701_create_user_table.js
+++ migrations/20210901215701_create_user_table.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex) {
-    return knex.schema.createTable('login_user' , t => {
+    return knex.schema.createTable('user' , t => {
         t.increments('id').unsigned().primary();
         t.string('email').notNull();
         t.string('password_digest').notNull();
